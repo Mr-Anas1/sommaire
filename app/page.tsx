75f032fafab481bc28d8be39e2053e185fc4ace5
Mr-Anas1 <a3689211@gmail.com>
2025-04-06 10:56:43 +0530

    Landing Page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import HeroSection from "@/components/common/Home/hero-section";
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/common/Home/demo-section";
+import HowItWorksSection from "@/components/common/Home/how-it-works";
+import PricingSection from "@/components/common/Home/pricing-section";
+import CTASection from "@/components/common/Home/cta-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection />
-      <HowItWorksSection />
-      <PricingSection />
-      <CTASection />
-       */}
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <CTASection />
+      </div>
     </div>
   );
 }
